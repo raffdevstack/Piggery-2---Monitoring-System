1875e284892b46f78658c51f9429f35b5bacb5f6
Raffy Dela Cruz <142416768+raffdevstack@users.noreply.github.com>
2025-01-03 01:47:16 +0800

    dht11 intro

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,30 @@
 #define BLYNK_TEMPLATE_NAME "Backyard Piggery"
 #define BLYNK_AUTH_TOKEN "9m5XLGdZ6dQTm8fXzHY88WwK9jQ1NfwC"
 #define BLYNK_PRINT Serial
+#define DHT11_PIN 4    // D2 (GPIO4) for Fan
 
 #include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h> // better lcd communication
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <DHT11.h>
 
-// const char* ssid = "Hotspot_ko";
-// const char* pass = "abcdefghij";
-const char* ssid = "LUNA BOARDING HOUSE PISO WIFI";
-const char* pass = "";
+const char* ssid = "Hotspot_ko";
+const char* pass = "abcdefghij";
+// const char* ssid = "LUNA BOARDING HOUSE PISO WIFI";
+// const char* pass = "";
 
 bool wifi_connected_state = false;
 bool blynk_connected_state = false;
+int temperature = 0;
+int humidity = 0;
+double heat_index_celsius = 0;
+int odor_level = 0;
 
 BlynkTimer timer;
-
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+DHT11 dht(DHT11_PIN);
 
 // put function declarations here:
 int myFunction(int, int);
@@ -28,6 +34,9 @@ void dispStartScreen();
 void connectToWifi();
 void lcdNotifier(String text);
 void connectToBlynk();
+void readSensors();
+void displaySensorPlaceholders();
+void displaySensorData();
 
 void setup() {
     // put your setup code here, to run once:
@@ -53,6 +62,9 @@ void setup() {
     Blynk.config(BLYNK_AUTH_TOKEN);
 
     timer.setInterval(10000L, connectToWifi);
+    timer.setInterval(2000L, readSensors);
+    timer.setInterval(5000L, displaySensorPlaceholders);
+    timer.setInterval(2100L, displaySensorData);
 
 }
 
@@ -142,5 +154,29 @@ void connectToBlynk() {
         Serial.println("Blynk online again!");
         blynk_connected_state = true;
     }
-        
+}
+
+void readSensors() {
+
+    int result = dht.readTemperatureHumidity(temperature, humidity);
+
+    if (result != 0) {
+        lcdNotifier("DHT11 error!");
+    }
+}
+
+void displaySensorPlaceholders() {
+    lcdPrinter(0,0, "Tmp:");
+    lcdPrinter(6,0, "C");
+    lcdPrinter(8,0, "Hmd:");
+    lcdPrinter(14,0, "%");
+    // second row
+    lcdPrinter(0,1, "Odor Lvl:");
+    lcdPrinter(12,1, "ppm");
+}
+
+void displaySensorData() {
+    lcdPrinter(4,0, String(temperature));
+    lcdPrinter(12,0, String(humidity));
+    lcdPrinter(9,1, String(odor_level));
 }
